38dc325d77796492d21c1e3ac252ae5e271ffc8c
EricWSS <eric.wss@gmail.com>
2025-02-08 13:56:55 -0300

    Limpeza e modificações nas aulas.

--- aula4/ty.js
+++
@@ -1,2 +0,0 @@
-const oi = 'diga'
-console.log(oi)
--- aula5/routers/users.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from routers.auth import get_current_user
-from models.user import User
-
-router = APIRouter()
-
-@router.get("/me")
-def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
